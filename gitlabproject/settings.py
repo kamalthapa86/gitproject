d62b086231ba51fb632365c395948c594ed249ff
KAMAL THAPA <48417274+kamalthapa86@users.noreply.github.com>
2024-12-20 12:46:35 +0530

    Update settings.py

--- gitlabproject/settings.py
+++ gitlabproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3djs-e-!c0b97%vkgq#!!h$is_p!=)zlqje@is^1+#3mw+hkv+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
